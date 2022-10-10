9ce9228b84dda84c972050a83a779a388fc8d41c
Soobin Rho <soobinrho@gmail.com>
2022-10-09 23:37:34 -0500

    Add better whitespace package
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- home/soobinrho/.config/nvim/lua/plugins.lua
+++ home/soobinrho/.config/nvim/lua/plugins.lua
@@ -19,7 +19,7 @@ return require('packer').startup(function(use)
   use 'nvim-lua/plenary.nvim'
   use 'terrortylor/nvim-comment'
   use 'andymass/vim-matchup'
-  use 'bronson/vim-trailing-whitespace'
+  use 'ntpeters/vim-better-whitespace'
   use 'editorconfig/editorconfig-vim'
   use 'lukas-reineke/indent-blankline.nvim'
   use 'norcalli/nvim-colorizer.lua'
